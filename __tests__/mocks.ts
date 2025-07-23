40c57b2ddbbc6e82f21e2faef2bd49dca127597b
Justin Lacy <justin.m.lacy@gmail.com>
2025-07-22 21:28:57 -0700

    more epitaphs and items

--- __tests__/mocks.ts
+++ __tests__/mocks.ts
@@ -12,3 +12,33 @@
 	});
 
 });*/
+
+import { ICache } from "rpg/util/icache";
+export type MockCache<T = any> = ICache<T> & { vars: Record<string, any> };
+export const MockCache = <T extends any>() => {
+
+	const Cache = vi.fn(function <T>(this: MockCache<T>): ICache<any> {
+		this.vars = {};
+		return this;
+	});
+	Cache.prototype.subcache = vi.fn(
+		function (this: typeof Cache) { return this }
+	);
+	Cache.prototype.cache = Cache.prototype.store = vi.fn(
+		function (this: MockCache, key: string, val: any) {
+			this.vars[key] = val;
+		});
+
+	Cache.prototype.get = vi.fn(function (this: MockCache<any>, key: string) {
+		return this.vars[key];
+	});
+
+	Cache.prototype.fetch = vi.fn(
+		async function (this: MockCache<any>, key: string) {
+			return this.vars[key];
+		}
+	);
+
+	return Cache;
+
+}
--- rpg/world/tests/world.test.ts
+++ rpg/world/tests/world.test.ts
@@ -1,32 +1,8 @@
+import { MockCache } from "__tests__/mocks";
 import { InitGame } from "rpg/init";
-import { ICache } from "rpg/util/icache";
 import { World } from "rpg/world/world";
 
-type MockCache<T = any> = ICache<T> & { vars: Record<string, any> };
-
 describe('Game World', async () => {
-
-	const Cache = vi.fn(function <T>(this: MockCache<T>): ICache<any> {
-		this.vars = {};
-		return this;
-	});
-	Cache.prototype.subcache = vi.fn(
-		function (this: typeof Cache) { return this }
-	);
-	Cache.prototype.cache = Cache.prototype.store = vi.fn(
-		function (this: MockCache, key: string, val: any) {
-			this.vars[key] = val;
-		});
-
-	Cache.prototype.get = vi.fn(function (this: MockCache<any>, key: string) {
-		return this.vars[key];
-	});
-
-	Cache.prototype.fetch = vi.fn(
-		async function (this: MockCache<any>, key: string) {
-			return this.vars[key];
-		}
-	);
 
 	beforeAll(async () => {
 		await InitGame();
@@ -38,15 +14,16 @@ describe('Game World', async () => {
 
 	test('Generate new world.', async () => {
 
-		const world = new World(new Cache(),
-			new Cache());
+		const Cache = MockCache();
+
+		const world = new World(new Cache(), new Cache());
 
 		await world.init();
 
 		const loc = world.getLoc({ x: 0, y: 0 });
 		expect(loc).toBeDefined();
-
 		expect(loc?.features.length).toBeGreaterThanOrEqual(2);
+		expect(Object.keys(loc!.exits).length).toBeGreaterThan(1);
 
 
 	});
